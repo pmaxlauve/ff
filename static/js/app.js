9839a55a5714525c11929bfb8f0c37a199717763
Max Lauve <pmaxlauve@gmail.com>
2021-08-20 11:53:16 -0500

    filter buttons update

--- static/js/app.js
+++ static/js/app.js
@@ -37,7 +37,7 @@ for (var i in data){
 }
 
 function addRow(obj){
-    var row = `<tr scope="row" class="test-row-${obj.id}">
+    var row = `<tr scope="row" class="test-row-${obj.id} pos-${obj.pos}">
                    <td><a href="https://www.fantasypros.com/nfl/players/${obj.name.toLowerCase().replaceAll(' ', '-')}.php" onClick="return popup(this, 'notes')">${obj.name}</a></td>
                    <td>${obj.pos}</td>
                    <td>${obj.rank}</td>
@@ -61,8 +61,92 @@ function addRow(obj){
     $(`#cancel-${obj.id}`).on('click', cancelDeletion)
     $(`#confirm-${obj.id}`).on('click', confirmDeletion)
     $(`#save-${obj.id}`).on('click', saveUpdate)
+
+    var all = d3.select(".filterAll")
+    var rb = d3.select(".filterRb")
+    var wr = d3.select(".filterWr")
+    var qb = d3.select(".filterQb")
+    var te = d3.select(".filterTe")
+    
+    
+    all.on('click', filterAll)
+    rb.on('click', filterRb)
+    wr.on('click', filterWr)
+    qb.on('click', filterQb)
+    te.on('click', filterTe)
+    
     
 
+
+function filterAll(){
+
+    var rbRow = $(`.pos-RB`)
+    var wrRow = $(`.pos-WR`)
+    var qbRow = $(`.pos-QB`)
+    var teRow = $(`.pos-TE`)
+
+    wrRow.removeClass('hidden')
+    qbRow.removeClass('hidden')
+    teRow.removeClass('hidden')
+    rbRow.removeClass('hidden')
+        
+}
+
+function filterQb(){
+
+    var rbRow = $(`.pos-RB`)
+    var wrRow = $(`.pos-WR`)
+    var qbRow = $(`.pos-QB`)
+    var teRow = $(`.pos-TE`)
+
+    wrRow.addClass('hidden')
+    qbRow.removeClass('hidden')
+    teRow.addClass('hidden')
+    rbRow.addClass('hidden')
+        
+}
+
+function filterWr(){
+
+    var rbRow = $(`.pos-RB`)
+    var wrRow = $(`.pos-WR`)
+    var qbRow = $(`.pos-QB`)
+    var teRow = $(`.pos-TE`)
+
+    wrRow.removeClass('hidden')
+    qbRow.addClass('hidden')
+    teRow.addClass('hidden')
+    rbRow.addClass('hidden')
+     
+}
+
+function filterTe(){
+
+    var rbRow = $(`.pos-RB`)
+    var wrRow = $(`.pos-WR`)
+    var qbRow = $(`.pos-QB`)
+    var teRow = $(`.pos-TE`)
+
+    wrRow.addClass('hidden')
+    qbRow.addClass('hidden')
+    teRow.removeClass('hidden')
+    rbRow.addClass('hidden')
+     
+}
+
+function filterRb(){
+
+    var rbRow = $(`.pos-RB`)
+    var wrRow = $(`.pos-WR`)
+    var qbRow = $(`.pos-QB`)
+    var teRow = $(`.pos-TE`)
+
+    wrRow.addClass('hidden')
+    qbRow.addClass('hidden')
+    teRow.addClass('hidden')
+    rbRow.removeClass('hidden')
+    
+}
 
     // $(`#result-${obj.id}`).on('click', editResult)
     
@@ -117,7 +201,7 @@ function popup(mylink, windowname) {
     if (! window.focus)return true; 
     var href; 
     if (typeof(mylink) == 'string') href=mylink; 
-    else href=mylink.href; window.open(href, windowname, 'width=800,height=1200,scrollbars=yes,top=150,left=900'); 
+    else href=mylink.href; window.open(href, windowname, 'width=1200,height=1200,scrollbars=yes,top=150,left=900'); 
     return false; }
 
 
